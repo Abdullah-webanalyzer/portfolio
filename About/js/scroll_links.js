a6fa132b79f885d6d55217681b2119f4975f511f
Abdullah-webanalyzer <abdullah.webanalyzer@gmail.com>
2025-06-09 16:33:52 +0500

    added dynamic margin top to external-body to make it visible under navbar.Also added styling to make context takes 100% of external div

--- About/js/scroll_links.js
+++ About/js/scroll_links.js
@@ -1,6 +1,7 @@
 document.addEventListener('DOMContentLoaded', function() {
     const navbarHeight = document.querySelector('.navbar').offsetHeight; // Get dynamic navbar height
     const navLinks = document.querySelectorAll('a[data-target]'); // Select links with data-target
+    const down_to_navbar = document.getElementById('down_to_navbar'); // Select the div with external-body class
 
     navLinks.forEach(link => {
         link.addEventListener('click', function(e) {
@@ -20,4 +21,6 @@ document.addEventListener('DOMContentLoaded', function() {
             }
         });
     });
+    const new_margin_top=navbarHeight; // Calculate new margin-top value
+    down_to_navbar.style.marginTop = `${new_margin_top}px`; // Adjust margin-top of the div with external-body class
 });
